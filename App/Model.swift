d0f6831e9351dde83c75b843d87cccde01932366
Matt Gallagher <matt@zqueue.com>
2020-09-12 15:19:17 +1000

    Model functional implementation

--- App/Model.swift
+++ App/Model.swift
@@ -15,11 +15,18 @@ struct Article: Codable {
 }
 
 class Model: ObservableObject {
-	@Published var feed: Feed? = Feed(
-		items: [
-			Article(url: URL(string: "mock://x/a1.html")!, title: "Article1", content: "One"),
-			Article(url: URL(string: "mock://x/a2.html")!, title: "Article2", content: "Two"),
-			Article(url: URL(string: "mock://x/a3.html")!, title: "Article3", content: "Three")
-		]
-	)
+	@Published var feed: Feed?
+	
+	var task: URLSessionDataTask?
+	init() {
+		let request = URLRequest(url: URL(string: "https://www.cocoawithlove.com/feed.json")!)
+		task = URLSession.shared.dataTask(with: request) { data, response, error in
+			do {
+				if let error = error { throw error }
+				let feed = try JSONDecoder().decode(Feed.self, from: data ?? Data())
+				DispatchQueue.main.async { self.feed = feed }
+			} catch {}
+		}
+		task?.resume()
+	}
 }
